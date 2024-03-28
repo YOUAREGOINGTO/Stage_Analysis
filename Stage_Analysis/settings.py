1534e27f5416f13e5a9a93db09038fac33caf8fc
YOUAREGOINGTO <yaswanthjogireddy02@gmail.com>
2024-03-28 23:44:11 +0530

    Hosting Changes

--- Stage_Analysis/settings.py
+++ Stage_Analysis/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'media')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+
 
 
 # Default primary key field type
--- Stage_Analysis/urls.py
+++ Stage_Analysis/urls.py
@@ -23,6 +23,8 @@ from core import views  # Import views from the core app
 from django.contrib import admin
 from django.urls import path, include
 from core import views  # Import views from the core app
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='root'),  # Use views from the core app
@@ -31,5 +33,7 @@ urlpatterns = [
 ]
 
 
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
 
 
